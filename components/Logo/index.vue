20d9e015eb60471ef8c53c33c159ef186a266e9b
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-04-09 01:47:39 +0800

    style: prevent drag image

--- components/Logo/index.vue
+++ components/Logo/index.vue
@@ -9,6 +9,7 @@ const imgSrc = computed(() => colorMode.value === 'dark' ? '/logo-dark.svg' : '/
     relative w-60 lg:w-64 xl:w-72
     mt-24 lg:mt-32 xl:mt-36 xl:mt-48
     mb-12 lg:mb-16
+    pointer-events-none select-none
   >
     <NuxtImg w-full :width="240" :height="240" :src="imgSrc" />
     <NuxtImg z--1 w-full absolute top-0 blur-2xl scale-130 :src="imgSrc" />
