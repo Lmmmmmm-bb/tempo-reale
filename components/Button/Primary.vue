d228688bf90241d70272dd5fd1eba7d37b7b7348
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-04-09 12:59:00 +0800

    style: focus and active text

--- components/Button/Primary.vue
+++ components/Button/Primary.vue
@@ -3,10 +3,10 @@
     bg="#222" text-white
     border="1 #222"
     hover="bg-white text-#222"
-    active="bg-#eaeaea"
+    active="bg-#eaeaea text-#222"
     dark="border-white text-#222 bg-white"
     dark:hover="bg-#222 text-white"
-    dark:active="bg-#333"
+    dark:active="bg-#333 text-white"
   >
     <slot />
   </Button>
--- components/Button/Secondary.vue
+++ components/Button/Secondary.vue
@@ -3,10 +3,10 @@
     bg-white text-black
     border="1 #eaeaea"
     hover="border-black"
-    active="bg-#eaeaea"
+    active="bg-#eaeaea border-black"
     dark="border-#333 text-#888 bg-#222"
     dark:hover="border-white text-white"
-    dark:active="bg-#333"
+    dark:active="bg-#333 border-white text-white"
   >
     <slot />
   </Button>
