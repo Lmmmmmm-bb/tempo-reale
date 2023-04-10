4d515202bd9722acbf566c03713026990787a2b7
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-04-10 08:02:28 +0000

    style: parallax logo

--- app.vue
+++ app.vue
@@ -1,11 +1,22 @@
+<script setup lang="ts">
+const target = ref<HTMLDivElement>();
+const parallax = reactive(useParallax(target));
+
+const rotate = computed(() => ({ x: parallax.roll * 30, y: parallax.tilt * 30 }));
+</script>
+
 <template>
   <div
+    ref="target"
     h-full
-    flex
-    flex-col
-    items-center
+    flex flex-col items-center
   >
-    <Logo />
+    <div perspective-500px>
+      <Logo
+        transition-all duration-500 ease-out
+        :style="{ transform: `rotateX(${rotate.x}deg) rotateY(${rotate.y}deg)` }"
+      />
+    </div>
     <div flex>
       <ClientOnly>
         <template #fallback>
